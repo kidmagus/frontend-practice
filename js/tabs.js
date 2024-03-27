39598e0309e8d75dc3141d11a51c72b64d98a341
kidmagus <msamson19@Outlook.com>
2024-03-27 15:56:35 +0800

    accordions and tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,13 @@
 
 // Variable for the each divs and li //
 const tabTarget = document.querySelectorAll('[data-tab-target]');
-const tabContents = document.querySelectorAll('[data-nav]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
 
 //For Loop // Get Attri of each target ..
 tabTarget.forEach(tabTargets => {
     const target = tabTargets.getAttribute('data-tab-target');
-    const targetContent = document.querySelector(`[data-nav="${target}"]`);
+    const targetContent = document.querySelector(`[data-tab-content="${target}"]`);
    
   
 // Function for Displaying the Content
